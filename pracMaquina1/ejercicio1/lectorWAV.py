2430094bbe7c3c7b068d655b9f4ce66711cb1a8d
Nicolino <nico262626las@gmail.com>
2025-08-30 17:10:47 -0300

    Ejercicio 1 terminado

--- pracMaquina1/ejercicio1/lectorWAV.py
+++ pracMaquina1/ejercicio1/lectorWAV.py
@@ -1,10 +1,15 @@
+#Se hace uso del módulo struct para desempaquetar los bytes leídos del archivo
 import struct
 
 def wav(ruta):
     with open (ruta, "rb") as wav:
+        #Leemos los primeros 12 bytes debido a que ese es el tamaño de la cabecera
         cabecera = wav.read(12)
+        #Desempaquetamos los bytes leídos en 3 variables, el argumento "<4sI4s" indica que se leerán 3 valores,
+        #el primero y el tercero son cadenas de 4 bytes y el segundo es un entero sin signo de 4 bytes
         riff, size, wave = struct.unpack("<4sI4s", cabecera)
         retorno = False
+        #Si riff y wave son iguales a "RIFF" y "WAVE" respectivamente, entonces el archivo es un WAV
         if riff == b"RIFF" and wave == b"WAVE":
             print(f"ChunID: {riff}, Size: {size}, Format: {wave}")
             retorno = True
