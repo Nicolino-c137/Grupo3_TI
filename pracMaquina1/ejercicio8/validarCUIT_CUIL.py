c5b82574fd35c9d086ab90d870e8fb8c6bbca8c8
Nicolino <nico262626las@gmail.com>
2025-09-01 18:15:58 -0300

    Terminado

--- pracMaquina1/ejercicio8/validarCUIT_CUIL.py
+++ pracMaquina1/ejercicio8/validarCUIT_CUIL.py
@@ -8,16 +8,20 @@ def validar(cuit):
         #El algoritmo módulo 11 utiliza una serie de pesos para cada posición del número base.
         base = [5, 4, 3, 2, 7, 6, 5, 4, 3, 2]
         sumatoria = 0
+        #Se calcula la sumatoria del producto de cada dígito por su peso correspondiente y se obtiene el resto de la división por 11
         for i in range(10):
             sumatoria += int(cuit[i])*base[i]
         resto = sumatoria%11
-        #Existen 3 reglas para determinar el dígito verificador, si el resto es 0, si el resto es 1 y si el resto es mayor a 1.
+        #Existen 3 reglas para determinar el dígito verificador
+        #Si el reto es 0, entonces el dígito verificador debe ser 0
         if resto == 0:
             retorno = 0 == int(cuit[10])
+        #Si el resto es 1, entonces se debe cambiar el prefijo del cUIT/CUIL a 23 y volver a calcular
         elif resto == 1:
             cuit[0] = "2"
             cuit[1] = "3"
-            retorno = validar(cuit)           
+            retorno = validar(cuit)
+        #Si el resto es cualquier otro número, entonces el dígito verificador debe ser 11 menos el resto          
         else:
             retorno = (11-resto) == int(cuit[10])
     return retorno
