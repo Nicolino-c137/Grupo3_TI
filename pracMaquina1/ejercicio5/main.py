7f4434fabfe671f150e27c03fbf1742c3a835c1a
Nicolino <nico262626las@gmail.com>
2025-09-06 20:49:23 -0300

    Terminado

--- pracMaquina1/ejercicio5/main.py
+++ pracMaquina1/ejercicio5/main.py
@@ -1,54 +1,145 @@
-#Se utilzo las librerias csv para manipular la base de datos y os para obtener el tamaño del archivo
-import csv
-import os
+import struct, csv, os
 
-#subprograma que se encarga de dar el formato correcto de los registros, es decir, los rellena con caracteres en blanco para cumplir con el tamaño
-def formatear_registro(apellido_nombre, direccion, dni, estudios_primarios, estudios_secundarios, estudios_universitarios, vivienda_propia, obra_social, trabaja, es_jubilado, tiene_hijos):
-    ayn=apellido_nombre.ljust(20)
-    d=direccion.ljust(30)
-    d_n_i=dni.ljust(8)
-    e_p=estudios_primarios
-    e_s=estudios_secundarios
-    e_u=estudios_universitarios
-    v_p=vivienda_propia
-    o_s=obra_social
-    t=trabaja
-    j=es_jubilado
-    h=tiene_hijos
-    return f"{ayn}{d}{d_n_i}{e_p}{e_s}{e_u}{v_p}{o_s}{t}{j}{h}"# devuelve todos los campos concatenados
-
-#subprograma que se encarga de guardar los datos del archivo csv en una lista
-def guardar_datos_en_lista(nombre_archivo):
-    lista = []
-    with open(nombre_archivo, mode='r', encoding='utf-8') as archivo_csv:
-        lector_csv = csv.DictReader(archivo_csv)
+#Guardamos los datos de las personas en una lista de diccionarios
+def getDatos():
+    personas = []
+    with open("PracMaquina1/ejercicio5/Personas.csv", "r", encoding="utf-8") as archi:
+        lector_csv = csv.DictReader(archi)
         for fila in lector_csv:
-            lista.append(fila)
-    return lista
+            personas.append(fila)
+    return personas
 
-#subprograma que se encaraga de recuperar los datos y mostrarlos
-#def recuperar_datosLF():
-    #print("---------Datos del archivo de longitud fija------------")
-    #with open("personasLF.dat", "r", encoding="utf-8") as f:
-    #    for linea in f:
-    #        print(f"Apellido y Nombre: {linea[0:19]}, Direccion: {linea[20:49]}, DNI: {linea[50:57]}, Estudios Primarios: {linea[58]}, Estudios Secundarios: {linea[59]}, Estudios Universitarios: {linea[60]}, Vivienda propia: {linea[61]}, Obra social: {linea[62]}, Trabaja: {linea[63]}, Jubilado: {linea[64]}, Hijos: {linea[65]} ")
+#Con esto empaquetamos los flags en un solo byte, si tenemos que un flag es "s" ponemos un 1 en la posicion correspondiente
+def packFlags(*flags):
+    byte = 0
+    for i, flag in enumerate(flags):
+        if flag == "s":
+            byte |= (1 << i)
+    return byte
 
-#subprograma que se encarga de mostrar el tamaño de los datos en bytes, utilizando funciones de la libreria os
-def tamaño_del_archivo(nombre_archivo):
-    print(f"El archivo {nombre_archivo} ocupa {os.path.getsize(nombre_archivo)} bytes")
+#Con esto desempaquetamos los flags de un byte a una lista de 8 elementos
+def unpackFlags(byte):
+    flags = []
+    for i in range(8):
+        flags.append((byte >> i) & 1)
+    return flags
 
-#subprograma que reliza el inciso a)
-def archivo_de_longitud_fija():
-    DatosLF=[]
-    lista=guardar_datos_en_lista("Personas.csv")
-    for fila in lista:
-        DatosLF.append(formatear_registro(fila['apellido_nombre'],fila['direccion'],fila['dni'],fila['estudios_primarios'],fila['estudios_secundarios'],fila['estudios_universitarios'],fila['vivienda_propia'],fila['obra_social'],fila['trabaja'],fila['es_jubilado'],fila['tiene_hijos']))
-    with open("personasLF.dat", "w", encoding="utf-8") as f: #funcion que crea el archivo personasLF.dat
-        for r in DatosLF:
-            f.write(r + "\n")
-    #recuperar_datosLF()
-    tamaño_del_archivo("personasLF.dat")
+#Escribimos los datos en un archivo binario con formato fijo
+def guardarFijo(personas):
+    #De esta forma decimos que apellido y nombre tiene 50 bytes, direccion 40, dni 8 y un byte para los flags
+    formato = "50s40s8sB"
+    with open("pracMaquina1/ejercicio5/fijos.dat", "wb") as f:
+        #Con ljust nos aseguramos que los strings tengan la longitud correcta, ya que estamos trabajando con un formato fijo
+        for p in personas:
+            apellidoYnombre = p["apellidoYnombre"].ljust(50)
+            direccion = p["direccion"].ljust(40)
+            dni = p["dni"].ljust(8)
+            flags = packFlags(p["estudiosPrimarios"], p["estudiosSecundarios"], p["estudiosUniversitarios"], p["viviendaPropia"], p["obraSocial"], p["trabaja"], p["esJubilado"], p["tieneHijos"])
+            #Codificamos los strings a bytes y los empaquetamos
+            registro = struct.pack(formato, apellidoYnombre.encode("utf-8"), direccion.encode("utf-8"), dni.encode("utf-8"), flags)
+            f.write(registro)
 
+#Recuperamos los datos del archivo binario con formato fijo         
+def leerFijo():
+    personas = []
+    formato = "50s40s8sB"
+    with open("pracMaquina1/ejercicio5/fijos.dat", "rb") as f:
+        #Mientras podamos leer un chunk del tamaño del formato, lo desempaquetamos y lo agregamos a la lista
+        while chunk := f.read(struct.calcsize(formato)):
+            apellidoYnombre, direccion, dni, flags = struct.unpack(formato, chunk)
+            #Con strip eliminamos los espacios en blanco que fueron agregados con ljust
+            apellidoYnombre = apellidoYnombre.decode("utf-8").strip()
+            direccion = direccion.decode("utf-8").strip()
+            dni = dni.decode("utf-8").strip()
+            flags = unpackFlags(flags)
+            personas.append({
+                "apellidoYnombre": apellidoYnombre,
+                "direccion": direccion,
+                "dni": dni,
+                "estudiosPrimarios": "s" if flags[0] else "n",
+                "estudiosSecundarios": "s" if flags[1] else "n",
+                "estudiosUniversitarios": "s" if flags[2] else "n",
+                "viviendaPropia": "s" if flags[3] else "n",
+                "obraSocial": "s" if flags[4] else "n",
+                "trabaja": "s" if flags[5] else "n",
+                "esJubilado": "s" if flags[6] else "n",
+                "tieneHijos": "s" if flags[7] else "n"
+            })
+    return personas
+        
+def listarPersonas(personas):
+    for p in personas:
+        print(f"""
+Apellido y Nombre: {p["apellidoYnombre"]}
+Direccion: {p["direccion"]}
+DNI: {p["dni"]}
+Estudios Primarios: {p["estudiosPrimarios"]}
+Estudios Secundarios: {p["estudiosSecundarios"]}
+Estudios Universitarios: {p["estudiosUniversitarios"]}
+Vivienda Propia: {p["viviendaPropia"]}
+Obra Social: {p["obraSocial"]}
+Trabaja: {p["trabaja"]}
+Es Jubilado: {p["esJubilado"]}
+Tiene Hijos: {p["tieneHijos"]}
+------------------------------""")
+
+#Escribimos los datos en un archivo binario con formato variable
+def guardarVariable(personas):
+    with open("pracMaquina1/ejercicio5/variable.dat", "wb") as f:
+        for p in personas:
+            apellidoYnombre = p["apellidoYnombre"].encode("utf-8")
+            direccion = p["direccion"].encode("utf-8")
+            dni = p["dni"].encode("utf-8")
+            flags = packFlags(p["estudiosPrimarios"], p["estudiosSecundarios"], p["estudiosUniversitarios"], p["viviendaPropia"], p["obraSocial"], p["trabaja"], p["esJubilado"], p["tieneHijos"])
+            #Guardamos la longitud de cada campo antes del campo en si
+            registro = struct.pack("B", len(apellidoYnombre)) + apellidoYnombre
+            registro += struct.pack("B", len(direccion)) + direccion
+            registro += struct.pack("B", len(dni)) + dni
+            registro += struct.pack("B", flags)
+            f.write(registro)
+          
+#Recuperamos los datos del archivo binario con formato variable  
+def leerVariable():
+    personas = []
+    with open("pracMaquina1/ejercicio5/variable.dat", "rb") as f:
+        while True:
+            #Leemos el primer campo, si no hay nada mas que leer, salimos del bucle
+            bof = f.read(1)
+            if not bof:
+                break
+            #Desempaquetamos la longitud del campo y luego leemos el campo
+            lenApellidoYnombre = struct.unpack("B", bof)[0]
+            apellidoYnombre = f.read(lenApellidoYnombre).decode("utf-8")
+            lenDireccion = struct.unpack("B", f.read(1))[0]
+            direccion = f.read(lenDireccion).decode("utf-8")
+            lenDni = struct.unpack("B", f.read(1))[0]
+            dni = f.read(lenDni).decode("utf-8")
+            flags = struct.unpack("B", f.read(1))[0]
+            flags = unpackFlags(flags)
+            personas.append({
+                "apellidoYnombre": apellidoYnombre,
+                "direccion": direccion,
+                "dni": dni,
+                "estudiosPrimarios": "s" if flags[0] else "n",
+                "estudiosSecundarios": "s" if flags[1] else "n",
+                "estudiosUniversitarios": "s" if flags[2] else "n",
+                "viviendaPropia": "s" if flags[3] else "n",
+                "obraSocial": "s" if flags[4] else "n",
+                "trabaja": "s" if flags[5] else "n",
+                "esJubilado": "s" if flags[6] else "n",
+                "tieneHijos": "s" if flags[7] else "n"
+            })
+    return personas
+            
 
 if __name__ == "__main__":
-    archivo_de_longitud_fija()
+    personas = getDatos()
+    guardarFijo(personas)
+    datos = leerFijo()
+    #listarPersonas(datos) #Descomentar para ver los datos leidos del archivo binario
+    guardarVariable(personas)
+    datos = leerVariable()
+    #listarPersonas(datos) #Descomentar para ver los datos leidos del archivo binario
+    print("Comparamos el tamaño de los archivos:")
+    print(f"Archivo con formato fijo: {os.path.getsize('pracMaquina1/ejercicio5/fijos.dat')} bytes")
+    print(f"Archivo con formato variable: {os.path.getsize('pracMaquina1/ejercicio5/variable.dat')} bytes")
