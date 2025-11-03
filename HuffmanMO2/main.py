5103127367f97b2d8b2ca9b04c7603e39dc99129
davidlsma <davidlede3@gmail.com>
2025-11-03 18:59:18 -0300

    Implement data loading and decompression
    Added data loading and decompression functionality.

--- HuffmanMO2/main.py
+++ HuffmanMO2/main.py
@@ -1,6 +1,17 @@
 from Compresor import Compresor
+from Descompresor import descomprimir
+import json
+
+def cargar_datos(archivo_salida="comprimido.txt"):
+    with open(archivo_salida, "r", encoding="utf-8") as f:
+        datos = json.load(f)
+    return datos["codigosAscii"], datos["arboles"], datos["comprimido"]
+
 
 if __name__ == "__main__":
     with open("archivo.txt", "r", encoding="utf-8") as f:
         texto = f.read()
     Compresor(texto)
+    codAscii, arboles, comprimido = cargar_datos("comprimido.txt")
+    texto_recuperado = descomprimir(comprimido, arboles, codAscii)
+    print("\nTexto descomprimido:", texto_recuperado)
