1d2623aa2a948964760c4292de5b92f53059e784
davidlsma <davidlede3@gmail.com>
2025-11-03 19:23:29 -0300

    Enhance comments in cargar_datos function
    Added comment to clarify file reading process.

--- HuffmanMO2/main.py
+++ HuffmanMO2/main.py
@@ -3,9 +3,9 @@ from Descompresor import descomprimir
 import json
 
 def cargar_datos(archivo_salida="comprimido.txt"):
-    with open(archivo_salida, "r", encoding="utf-8") as f:
+    with open(archivo_salida, "r", encoding="utf-8") as f: # Abrimos el archivo y cargamos el contenido JSON
         datos = json.load(f)
-    return datos["codigosAscii"], datos["arboles"], datos["comprimido"]
+    return datos["codigosAscii"], datos["arboles"], datos["comprimido"] # Devolvemos los tres elementos necesarios
 
 
 if __name__ == "__main__":
